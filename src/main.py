ba94b27b520740473dfbf616b4edd4c1857047eb
Your Name <you@example.com>
2025-02-09 01:47:59 +0100

    ..

--- src/main.py
+++ src/main.py
@@ -1,3 +1,32 @@
 import logging
 logging.basicConfig(level=logging.DEBUG)
 log = logging.getLogger(__name__)
+
+
+
+class App:
+	pass
+
+
+
+class Source:
+	pass
+
+class Directory(Source):
+	pass
+
+class MotionPostgress(Source):
+	pass
+
+
+
+class Focus:
+	pass
+
+class Area(Focus):
+	pass
+
+class User(Focus):
+	pass
+
+
